0a53cc8763d7a36ba092d5b6697ef5427d4dff33
zakary <askarru@gmail.com>
2013-08-14 03:55:57 -0800

    Add colvis

--- SchoolApp/App_Start/BundleConfig.cs
+++ SchoolApp/App_Start/BundleConfig.cs
@@ -26,7 +26,8 @@ namespace SchoolApp
 
             bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
                    "~/scripts/jquery.dataTables*",
-                    "~/scripts/datatables.bootstrap*"));
+                    "~/scripts/datatables.bootstrap*",
+                    "~/Scripts/ColVis.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/multi-select").Include("~/scripts/jquery.multi-select*"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
@@ -50,6 +51,7 @@ namespace SchoolApp
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         //"~/Content/themes/base/jquery.ui.theme.css",
                         "~/Content/themes/custom-theme/jquery-ui-1.10.2.custom.css"));
+
             bundles.Add(new StyleBundle("~/Content/font-awesome").Include("~/Content/font-awesome*"));
             bundles.Add(new StyleBundle("~/Content/multi-select").Include("~/Content/multi-select*"));
             bundles.Add(new StyleBundle("~/Content/fullcalendar").Include("~/Content/fullcalendar.css"));
@@ -58,14 +60,12 @@ namespace SchoolApp
             bundles.Add(new ScriptBundle("~/bundles/colorpicker").Include("~/Scripts/bootstrap-colorpicker.js"));
             bundles.Add(new StyleBundle("~/content/colorpicker").Include("~/Content/bootstrap-colorpicker.css"));
             bundles.Add(new ScriptBundle("~/bundles/quicksearch").Include("~/Scripts/jquery.quicksearch.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/chosen").Include("~/Scripts/chosen.jquery.js"));
             bundles.Add(new StyleBundle("~/content/chosen").Include("~/Content/chosen.css"));
-
             bundles.Add(new ScriptBundle("~/bundles/blockUI").Include("~/Scripts/blockUI.js"));
-
             bundles.Add(new ScriptBundle("~/bundles/webcam").Include("~/Scripts/jquery.webcam.js"));
             bundles.Add(new ScriptBundle("~/bundles/cookie").Include("~/Scripts/jquery.cookie.js"));
+            bundles.Add(new StyleBundle("~/content/colvis").Include("~/Content/ColVis.css"));
         }
     }
 }
